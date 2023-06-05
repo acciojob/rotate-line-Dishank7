7ab2c8caa986e78603cf8210131907c9209932d5
Dishank7 <127612933+Dishank7@users.noreply.github.com>
2023-06-05 23:26:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+let btn = document.getElementById("btn");
+let container = document.getElementsByClassName("container")[0];
+
+function start(){
+    container.innerText = "";
+    let line = document.createElement("div");
+    line.id = "line";
+    container.append(line);
+};
